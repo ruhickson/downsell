541909994521be0490fd6a84257a71ec8a0c2f48
arklow.sei <ru.hickson@gmail.com>
2025-05-06 23:41:01 +0100

    Add CSV upload and table display feature using PapaParse

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import Papa from 'papaparse'
+
+const csvData = ref([])
+const headers = ref([])
+
+function handleFileUpload(event) {
+  const file = event.target.files[0]
+  if (!file) return
+  Papa.parse(file, {
+    header: true,
+    skipEmptyLines: true,
+    complete: (results) => {
+      headers.value = results.meta.fields
+      csvData.value = results.data
+    }
+  })
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div style="max-width: 900px; margin: 2rem auto;">
+    <h1>CSV Table Viewer</h1>
+    <input type="file" accept=".csv" @change="handleFileUpload" />
+    <table v-if="csvData.length" border="1" cellpadding="8" style="margin-top: 2rem; width: 100%; border-collapse: collapse;">
+      <thead>
+        <tr>
+          <th v-for="header in headers" :key="header">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, idx) in csvData" :key="idx">
+          <td v-for="header in headers" :key="header">{{ row[header] }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else style="margin-top: 2rem; color: #888;">No data loaded. Please upload a CSV file.</p>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+table th {
+  background: #f4f4f4;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+table td, table th {
+  text-align: left;
 }
 </style>
